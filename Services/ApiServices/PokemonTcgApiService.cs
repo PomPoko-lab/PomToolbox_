4e9852f2f533d8a8deee013f306b2e3ab693579e
Kenny Cao <ckennx@gmail.com>
2025-02-17 14:11:04 -0600

    added get sets in pokemon service

--- Services/ApiServices/PokemonTcgApiService.cs
+++ Services/ApiServices/PokemonTcgApiService.cs
@@ -1,9 +1,12 @@
 namespace PomToolbox.Services.ApiServices;
 
+using System.Text;
 using PokemonTcgSdk.Standard.Features.FilterBuilder.Pokemon;
+using PokemonTcgSdk.Standard.Features.FilterBuilder.Set;
 using PokemonTcgSdk.Standard.Infrastructure.HttpClients;
 using PokemonTcgSdk.Standard.Infrastructure.HttpClients.Base;
 using PokemonTcgSdk.Standard.Infrastructure.HttpClients.Cards;
+using PokemonTcgSdk.Standard.Infrastructure.HttpClients.Set;
 using PomToolbox.Services.Interfaces;
 
 public class PokemonTcgApiService : IPokemonTcgApiService
@@ -18,19 +21,41 @@ public class PokemonTcgApiService : IPokemonTcgApiService
 
     public async Task<List<PokemonCard>> GetPokemonByName(string name)
     {
-        try 
-        {
+        try {
             var filter = PokemonFilterBuilder.CreatePokemonFilter().AddName(name + "*");
             ApiResourceList<PokemonCard> response = await this.pokeclient.GetApiResourceAsync<PokemonCard>(filter);
             List<PokemonCard> cards = response.Results;
-            if (cards.Count == 0)
-            {
-                return [];
-            }
+
             return cards;
+        } catch (Exception ex) {
+            Console.WriteLine($"API Error: {ex.Message}");
+            return [];
         }
-        catch (Exception ex)
-        {
+    }
+
+    public async Task<List<Set>> GetSets() {
+        try {
+            var filter = SetFilterBuilder.CreateSetFilter();
+            List<string> series = [
+                "Sword & Shield",
+                "Scarlet & Violet",
+                "Sun & Moon",
+                "XY",
+            ];
+
+            var seriesFilterBuilder = new StringBuilder();
+            foreach (string s in series) {
+                seriesFilterBuilder.Append(s).Append(',');
+            }
+
+            string seriesFilter = seriesFilterBuilder.ToString().TrimEnd(',');
+            filter.Add("series", seriesFilter);
+
+            ApiResourceList<Set> response = await this.pokeclient.GetApiResourceAsync<Set>(filter);
+            List<Set> sets = response.Results;
+
+            return sets;
+        } catch (Exception ex) {
             Console.WriteLine($"API Error: {ex.Message}");
             return [];
         }
--- Services/Interfaces/IPokemonTcgApiService.cs
+++ Services/Interfaces/IPokemonTcgApiService.cs
@@ -1,9 +1,11 @@
 using PokemonTcgSdk.Standard.Infrastructure.HttpClients.Cards;
+using PokemonTcgSdk.Standard.Infrastructure.HttpClients.Set;
 
 namespace PomToolbox.Services.Interfaces
 {
     public interface IPokemonTcgApiService
     {
         Task<List<PokemonCard>> GetPokemonByName(string name);
+        Task<List<Set>> GetSets();
     }
 }
