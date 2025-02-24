7201e721cf013ac41a2145f08eea9b1b70979448
Kenny Cao <ckennx@gmail.com>
2025-02-24 22:15:42 +0000

    wip creating the cards

--- PomToolbox/Data/Models/PokemonCard.cs
+++ PomToolbox/Data/Models/PokemonCard.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PomToolbox.Data.Models;
 
-public class PokemonCollection
+public class PokemonCard
 {
     [Key]
     public int Id { get; set; }
@@ -12,6 +12,17 @@ public class PokemonCollection
     [Required]
     [MaxLength(50)]
     public string Name { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(32)]
+    public string Set { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(32)]
+    public string Series { get; set; } = string.Empty;
+
+
+    public double AverageTcgPlayerPrice { get; set;} = 0.00;
 
     public DateTime DateCreated { get; set; } = DateTime.Now;
 }
