5473d2bc6deb1904dcd7e3f1fb0164452641b24c
Kenny Cao <ckennx@gmail.com>
2025-02-24 19:24:41 +0000

    added the database service and an interface to make future services quicker

---
+++ PomToolbox/Services/PokemonCollectionService.cs
@@ -0,0 +1,43 @@
+namespace PomToolbox.Services;
+using Microsoft.EntityFrameworkCore;
+using PomToolbox.Data.Models;
+using PomToolbox.Services.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public class PokemonCollectionService : IDatabaseService<PokemonCollection> {
+    private readonly ApplicationDbContext _db;
+
+    public PokemonCollectionService(ApplicationDbContext db) {
+        this._db = db;
+    }
+
+    public async Task<PokemonCollection> Create(PokemonCollection collection) {
+        await this._db.PokemonCollections.AddAsync(collection);
+        await this._db.SaveChangesAsync();
+        return collection;
+    }
+
+    public async Task<List<PokemonCollection>> Get() {
+        return await this._db.PokemonCollections.ToListAsync();
+    }
+
+    public async Task<PokemonCollection?> Get(int id) {
+        return await this._db.PokemonCollections.FindAsync(id);
+    }
+
+    public async Task<PokemonCollection> Update(PokemonCollection collection) {
+        this._db.PokemonCollections.Update(collection);
+        await this._db.SaveChangesAsync();
+        return collection;
+    }
+
+    public async Task Delete(PokemonCollection collection) {
+        this._db.PokemonCollections.Remove(collection);
+        await this._db.SaveChangesAsync();
+    }
+
+    // public async Task<List<PokemonCollection>> GetByUserId(int userId) {
+    //     return await this._db.PokemonCollections.Where(x => x.UserId == userId).ToListAsync();
+    // }
+}
