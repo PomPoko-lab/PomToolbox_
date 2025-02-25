b5846af15b4aad2c69fcbefcfc16077db046d07b
Kenny Cao <ckennx@gmail.com>
2025-02-25 18:34:54 +0000

    added the pokemoncards repositories

---
+++ PomToolbox/Data/Repositories/PokemonCardRepository.cs
@@ -0,0 +1,39 @@
+namespace PomToolbox.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using PomToolbox.Data.Models;
+using PomToolbox.Services.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public class PokemonCardRepository : IPokemonCardRepository {
+    private readonly ApplicationDbContext _db;
+
+    public PokemonCardRepository(ApplicationDbContext db) {
+        this._db = db;
+    }
+
+    public async Task<PokemonCard> Create(PokemonCard card) {
+        await this._db.PokemonCards.AddAsync(card);
+        await this._db.SaveChangesAsync();
+        return card;
+    }
+
+    public async Task<List<PokemonCard>> Get() {
+        return await this._db.PokemonCards.ToListAsync();
+    }
+
+    public async Task<PokemonCard?> Get(int id) {
+        return await this._db.PokemonCards.FindAsync(id);
+    }
+
+    public async Task<PokemonCard> Update(PokemonCard card) {
+        this._db.PokemonCards.Update(card);
+        await this._db.SaveChangesAsync();
+        return card;
+    }
+
+    public async Task Delete(PokemonCard card) {
+        this._db.PokemonCards.Remove(card);
+        await this._db.SaveChangesAsync();
+    }
+}
---
+++ PomToolbox/Services/Interfaces/IPokemonCardRepository.cs
@@ -0,0 +1,12 @@
+namespace PomToolbox.Services.Interfaces;
+
+using System.Collections.Generic;
+using PomToolbox.Data.Models;
+
+public interface IPokemonCardRepository {
+    public Task<PokemonCard> Create(PokemonCard card);
+    public Task<List<PokemonCard>> Get();
+    public Task<PokemonCard?> Get(int id);
+    public Task<PokemonCard> Update(PokemonCard card);
+    public Task Delete(PokemonCard card);
+}
